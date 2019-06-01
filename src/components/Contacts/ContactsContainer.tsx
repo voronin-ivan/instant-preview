7516bf6c565051b7e9e8a3613123aa697728b684
nage <pownedy@ya.ru>
2019-06-01 21:08:58 +0300

    fix typings in ContactsContainer

--- src/components/Contacts/ContactsContainer.tsx
+++ src/components/Contacts/ContactsContainer.tsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { Contacts, ContactsProps } from './Contacts';
 import { RootModel } from '../../models/root';
 
-const mapStateToProps = (state: RootModel): Partial<ContactsProps> => ({
+const mapStateToProps = (state: RootModel): ContactsProps => ({
     lang: state.lang,
 });
 
