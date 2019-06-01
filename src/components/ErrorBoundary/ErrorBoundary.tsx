087908e5f21788d2ff3c337f76e9442a2e0ec66e
nage <pownedy@ya.ru>
2019-06-01 21:08:34 +0300

    add ErrorBoundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import i18n from '../../utils/i18n';
+import { LangModel } from '../../models/lang';
+
+import './ErrorBoundary.scss';
+
+export interface ErrorBoundaryProps {
+    lang: LangModel;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    state = {
+        hasError: false,
+    }
+
+    static getDerivedStateFromError() {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, info: React.ErrorInfo) {
+        console.error(error, info);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <div className="error">
+                    <span>
+                        <span>{i18n('errorMain')}&nbsp;—&nbsp;</span>
+                        <a
+                            href="mailto:hi@insta-preview.com"
+                            className="error__link"
+                        >
+                            hi@insta-preview.com
+                        </a>
+                    </span>
+                </div>
+            );
+        }
+
+        return this.props.children;
+    }
+}
